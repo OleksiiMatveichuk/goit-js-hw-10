546489ab42c1ad9d21442ac5c356a9f428e0cc02
OleksiiMatveichuk <126767121+OleksiiMatveichuk@users.noreply.github.com>
2023-05-08 18:01:46 +0300

    add liberty

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 import './css/styles.css';
+var debounce = require('lodash.debounce');
+import Notiflix from 'notiflix';
+import { fetchCountries } from './fetchCountries';
 
 const DEBOUNCE_DELAY = 300;
