f1845678fcfdbb2ae20648cf1713681a5edbc603
OleksiiMatveichuk <126767121+OleksiiMatveichuk@users.noreply.github.com>
2023-05-08 23:03:50 +0300

    clb

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,1 +1,8 @@
-export const fetchCountries = name => {};
+const URL = 'https://restcountries.com/v3.1/';
+const FILTER = '?fields=name,flags,capital,languages,population';
+
+export const fetchCountries = clb => {
+  return fetch(`${URL}all${FILTER}`)
+    .then(res => res.json())
+    .then(arr => clb(arr));
+};
--- src/index.js
+++ src/index.js
@@ -1,6 +1,38 @@
 import './css/styles.css';
-var debounce = require('lodash.debounce');
+import debounce from 'lodash.debounce';
 import Notiflix from 'notiflix';
 import { fetchCountries } from './fetchCountries';
 
 const DEBOUNCE_DELAY = 300;
+const input = document.querySelector('#search-box');
+const list = document.querySelector('.country-list');
+const infoBlock = document.querySelector('.country-info');
+
+// const inputCountries = evt => {
+//   const result = fetchCountries()
+//     .then(res => res.json())
+//     .then(console.log);
+//   console.log('result :>> ', result);
+// };
+
+const addItem = arr => {
+  console.log('arr :>> ', arr);
+  arr.map(el => console.log('el :>> ', el));
+};
+
+fetchCountries(addItem);
+input.addEventListener('input', debounce(inputCountries, DEBOUNCE_DELAY));
+
+// *
+// *
+// *
+// *
+
+// name.official - повна назва країни
+// capital - столиця
+// population - населення
+// flags.svg - посилання на зображення прапора
+// languages - масив мов
+
+// Notiflix.Notify.init('hello');
+// success/failure/warning/info;
