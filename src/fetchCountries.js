bd1c59482bf2646d21432e8bb354c6e797b138a8
OleksiiMatveichuk <126767121+OleksiiMatveichuk@users.noreply.github.com>
2023-05-09 17:14:02 +0300

    not add style

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,8 +1,11 @@
 const URL = 'https://restcountries.com/v3.1/';
 const FILTER = '?fields=name,flags,capital,languages,population';
 
-export const fetchCountries = clb => {
-  return fetch(`${URL}all${FILTER}`)
-    .then(res => res.json())
-    .then(arr => clb(arr));
+export const fetchCountries = country => {
+  return fetch(`${URL}name/${country}${FILTER}`).then(res => {
+    if (!res.ok) {
+      return Promise.reject(res.json());
+    }
+    return res.json();
+  });
 };
--- src/index.js
+++ src/index.js
@@ -6,33 +6,53 @@ import { fetchCountries } from './fetchCountries';
 const DEBOUNCE_DELAY = 300;
 const input = document.querySelector('#search-box');
 const list = document.querySelector('.country-list');
-const infoBlock = document.querySelector('.country-info');
 
-// const inputCountries = evt => {
-//   const result = fetchCountries()
-//     .then(res => res.json())
-//     .then(console.log);
-//   console.log('result :>> ', result);
-// };
-
-const addItem = arr => {
-  console.log('arr :>> ', arr);
-  arr.map(el => console.log('el :>> ', el));
+const inputCountries = evt => {
+  const country = evt.target.value.trim();
+  if (!country) {
+    return;
+  }
+  fetchCountries(country)
+    .then(nameFilter)
+    .catch(() =>
+      Notiflix.Notify.failure('Oops, there is no country with that name')
+    );
 };
 
-fetchCountries(addItem);
+function nameFilter(arr) {
+  if (arr.length > 10) {
+    list.innerHTML = '';
+    return Notiflix.Notify.info(
+      'Too many matches found. Please enter a more specific name.'
+    );
+  }
+  if (arr.length <= 10 && arr.length > 1) {
+    const result = arr.reduce(
+      (acc, el) =>
+        acc +
+        `<li>
+        <img src="${el.flags.svg}" width ="40">
+        <h2>${el.name.official}</h2>
+    </li>`,
+      ''
+    );
+    return (list.innerHTML = result);
+  }
+  if (arr.length === 1) {
+    const result = arr.reduce(
+      (acc, el) =>
+        acc +
+        `<li>
+        <img src="${el.flags.svg}" width ="40">
+        <h2>${el.name.official}</h2>
+        <h3>Capital: ${el.capital}</h3>
+        <h3>Population: ${el.population}</h3>
+        <h3>Languages: ${el.languages}</h3>
+    </li>`,
+      ''
+    );
+    return (list.innerHTML = result);
+  }
+}
+
 input.addEventListener('input', debounce(inputCountries, DEBOUNCE_DELAY));
-
-// *
-// *
-// *
-// *
-
-// name.official - повна назва країни
-// capital - столиця
-// population - населення
-// flags.svg - посилання на зображення прапора
-// languages - масив мов
-
-// Notiflix.Notify.init('hello');
-// success/failure/warning/info;
